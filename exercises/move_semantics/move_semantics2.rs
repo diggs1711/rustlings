bcfce5215119f23ac890958789b2fab335376c5f
David Higgins <david.higgins@ovoenergy.com.au>
2021-04-11 14:10:21 +1000

    Initial commit

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,15 +2,14 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
+    let x = vec0.to_vec();
 
     let mut vec1 = fill_vec(vec0);
 
     // Do not change the following line!
-    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+    println!("{} has length {} content `{:?}`", "vec0", x.len(), x);
 
     vec1.push(88);
 
@@ -18,11 +17,11 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut x = vec.to_vec();
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    x.push(22);
+    x.push(44);
+    x.push(66);
 
-    vec
+    x
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn ..... {
+fn calculate_apple_price(val: i32) -> i32 {
+    if val <= 40 {
+        val * 2
+    } else {
+        val * 1
+    }
+}
 
 // Don't modify this function!
 #[test]
